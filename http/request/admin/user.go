594465b8d6625c24a202318544283d6bb2c2f7f7
laiyouxing <151647251+laiyouxing@users.noreply.github.com>
2026-07-22 13:51:29 +0800

    feat: 添加 InvitationBatchForm 及恢复完整文件内容

--- http/request/admin/user.go
+++ http/request/admin/user.go
@@ -17,8 +17,89 @@ type UserForm struct {
 	ExpiredAt int64            `json:"expired_at"`
 }
 
-func (uf *UserForm) FromUser(user *model.User) *UserForm { ... }
-func (uf *UserForm) ToUser() *model.User { ... }
+func (uf *UserForm) FromUser(user *model.User) *UserForm {
+	uf.Id = user.Id
+	uf.Username = user.Username
+	uf.Nickname = user.Nickname
+	uf.Email = user.Email
+	uf.Avatar = user.Avatar
+	uf.GroupId = user.GroupId
+	uf.IsAdmin = user.IsAdmin
+	uf.Status = user.Status
+	uf.Remark = user.Remark
+	uf.ExpiredAt = user.ExpiredAt
+	return uf
+}
+func (uf *UserForm) ToUser() *model.User {
+	user := &model.User{}
+	user.Id = uf.Id
+	user.Username = uf.Username
+	user.Nickname = uf.Nickname
+	user.Email = uf.Email
+	user.Avatar = uf.Avatar
+	user.GroupId = uf.GroupId
+	user.IsAdmin = uf.IsAdmin
+	user.Status = uf.Status
+	user.Remark = uf.Remark
+	user.ExpiredAt = uf.ExpiredAt
+	return user
+}
+
+type PageQuery struct {
+	Page     uint `form:"page"`
+	PageSize uint `form:"page_size"`
+}
+
+type UserQuery struct {
+	PageQuery
+	Username string `form:"username"`
+	GroupId  uint   `form:"group_id"`
+}
+type UserPasswordForm struct {
+	Id       uint   `json:"id" validate:"required"`
+	Password string `json:"password" validate:"required,gte=4,lte=32"`
+}
+
+type ChangeCurPasswordForm struct {
+	OldPassword string `json:"old_password" validate:"required,gte=4,lte=32"`
+	NewPassword string `json:"new_password" validate:"required,gte=4,lte=32"`
+}
+type GroupUsersQuery struct {
+	IsMy   int  `json:"is_my"`
+	UserId uint `json:"user_id"`
+}
+
+type MfaEnableForm struct {
+	Code string `json:"code" validate:"required" label:"动态码"`
+}
+
+type MfaDisableForm struct {
+	Password string `json:"password" validate:"required" label:"密码"`
+}
+
+type MfaResetForm struct {
+	UserId uint `json:"user_id" validate:"required" label:"用户ID"`
+}
+
+type RegisterForm struct {
+	Username        string `json:"username" validate:"required,gte=2,lte=32"`
+	Email           string `json:"email"`
+	Password        string `json:"password" validate:"required,gte=4,lte=32"`
+	ConfirmPassword string `json:"confirm_password" validate:"required,gte=4,lte=32"`
+	InviteCode      string `json:"invite_code"`
+}
+
+type UserTokenBatchDeleteForm struct {
+	Ids []uint `json:"ids" validate:"required"`
+}
+
+type InvitationForm struct {
+	Code          string `json:"code"`
+	MaxUsers      int    `json:"max_users"`
+	ExpiredAt     int64  `json:"expired_at"`
+	UserExpiredAt int64  `json:"user_expired_at"`
+	Remark        string `json:"remark"`
+}
 
 type InvitationDeleteForm struct {
 	Id uint `json:"id" validate:"required"`
