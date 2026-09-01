d4026b35f2a84fda7de080235905d314680a9162
laiyouxing <151647251+laiyouxing@users.noreply.github.com>
2026-09-01 14:25:19 +0800

    build: 同步 web-admin 产物（修复版本推送编辑保存）

--- resources/admin/static/chunk/clipboard-B6F-Xf0O.js
+++
@@ -1,1 +0,0 @@
-import{ah as a}from"./__vendor-LtSsU5Wq.js";import{T as s}from"../entry/index-BWE4qEbI.js";import{b as e}from"./_element-plus-B9QtHUnP.js";function n(t,r){const o=new a(r.target.toString(),{text:()=>t});o.on("success",()=>{e.success(s("CopySuccess")),o.destroy()}),o.on("error",()=>{e.error(s("CopyFailed")),o.destroy()}),o.onClick(r)}export{n as h};
--- resources/admin/static/chunk/global-B_A27N5m.js
+++
@@ -1,1 +0,0 @@
-import{o as a}from"../entry/index-BWE4qEbI.js";import{r as e}from"./_@vue-DVF7pgcZ.js";function c(){const s=e([]);return{allUsers:s,getAllUsers:async()=>{const t=await a({page_size:9999}).catch(r=>!1);t&&(s.value=t.data.list)}}}export{c as l};
